2b2695f3f4f8972e145c8c886dfe55b609dc729e
belligerentbeagle <ethan.wei@u.nus.edu>
2024-04-04 22:11:08 +0800

    Remove additional space

--- src/main/java/seedu/address/logic/commands/EditPersonCommand.java
+++ src/main/java/seedu/address/logic/commands/EditPersonCommand.java
@@ -57,7 +57,6 @@ public class EditPersonCommand extends Command {
 
     public static final String MESSAGE_EDIT_PERSON_SUCCESS = "Edited student: %1$s";
     public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided.";
-
     private final Index index;
     private final EditPersonDescriptor editPersonDescriptor;
 
