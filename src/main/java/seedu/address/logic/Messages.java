829408fe160f34e4cca76730c0e55dc3d13e8f7d
Titus Chew <39845485+tituschewxj@users.noreply.github.com>
2024-04-05 06:53:11 +0800

    Use NUSNet ID for consistency

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -34,7 +34,7 @@ public class Messages {
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The student's index provided is invalid";
-    public static final String MESSAGE_MISSING_NUSNET = "No such student with the NUSNET_ID found in contacts!";
+    public static final String MESSAGE_MISSING_NUSNET = "No such student with the NUSNet ID found in contacts!";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d students listed!";
     public static final String MESSAGE_DUPLICATE_FIELDS =
             "Multiple values specified for the following single-valued field(s): ";
