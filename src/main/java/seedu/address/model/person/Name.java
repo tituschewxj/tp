19d342b69983636737d4a98e222ed44f9993d5cd
saiutkarsh33 <122259179+saiutkarsh33@users.noreply.github.com>
2024-04-04 23:35:15 +0800

    Name Autocorrect

--- src/main/java/seedu/address/model/person/Name.java
+++ src/main/java/seedu/address/model/person/Name.java
@@ -16,7 +16,8 @@ public class Name {
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+    public static final String VALIDATION_REGEX = "[A-Z][a-z]*(\\s[A-Z][a-z]*)*";
+
 
     public final String fullName;
 
@@ -56,7 +57,7 @@ public class Name {
         }
 
         Name otherName = (Name) other;
-        return fullName.equals(otherName.fullName);
+        return fullName.toUpperCase().equals(otherName.fullName.toUpperCase());
     }
 
     @Override
